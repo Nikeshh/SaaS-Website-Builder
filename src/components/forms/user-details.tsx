f254538d567f6933c0ad6cfdf99db448eeeed89b
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-02-06 17:07:51 -0500

    Typo

--- src/components/forms/user-details.tsx
+++ src/components/forms/user-details.tsx
@@ -309,7 +309,7 @@ const UserDetails = ({ id, type, subAccounts, userData }: Props) => {
                       </SelectTrigger>
                     </FormControl>
                     <SelectContent>
-                      <SelectItem value="AGENCY_ADMING">
+                      <SelectItem value="AGENCY_ADMIN">
                         Agency Admin
                       </SelectItem>
                       {(data?.user?.role === 'AGENCY_OWNER' ||
