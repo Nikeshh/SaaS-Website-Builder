48eefc5c2ad4a37f1e3722a87a19b4ac7acc3c5a
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-02-07 21:58:32 -0500

    Stripe implementation

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -101,3 +101,23 @@ export const FunnelPageSchema = z.object({
   name: z.string().min(1),
   pathName: z.string().optional(),
 })
+
+export type Address = {
+  city: string
+  country: string
+  line1: string
+  postal_code: string
+  state: string
+}
+
+export type ShippingInfo = {
+  address: Address
+  name: string
+}
+
+export type StripeCustomerType = {
+  email: string
+  name: string
+  shipping: ShippingInfo
+  address: Address
+}
