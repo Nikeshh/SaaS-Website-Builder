b24395ac1798cff31db41fac240fda8d8c7b376d
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-02-08 13:59:59 -0500

    Stripe oauth url update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,5 +9,6 @@ export function getStripeOAuthLink(
   accountType: 'agency' | 'subaccount',
   state: string
 ) {
-  return `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_STRIPE_CLIENT_ID}&scope=read_write&redirect_uri=${process.env.NEXT_PUBLIC_URL}${accountType}&state=${state}`
+  return `https://connect.stripe.com/oauth/authorize?redirect_uri=${process.env.NEXT_PUBLIC_URL}${accountType}&client_id=${process.env.NEXT_PUBLIC_STRIPE_CLIENT_ID}&state=${state}&response_type=code&scope=read_write`;
 }
+
